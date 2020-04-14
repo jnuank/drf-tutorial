f82c25232117104d426538c121825611ec13b226
Jun Nakajima <ikuta1919@gmail.com>
2020-04-14 18:40:41 +0900

    add: vuecliでfrontendを追加

--- config/settings.py
+++ config/settings.py
@@ -133,5 +133,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta((minutes=30),
 }
